57e62ab98e1a88ae11e3424bc21a12e29bf9e9fd
Anchy <mongush.anchy17@gmail.com>
2024-03-26 10:40:33 +0300

    Add //new project

---
+++ project/1.1 - 3ISP920_Mongush_Gevorkyan/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "1.1 - 3ISP920_Mongush_Gevorkyan"
+include(":app")
+ 
